cec52b705a897e92cefa92772c2f95ec09c8de0c
biaOkVQM <113008015+MykhailoMusiienkoMosig@users.noreply.github.com>
2023-11-28 13:33:58 +0100

    Loss functions

---
+++ NeuralNetwork_with_CUDA/losses.cpp
@@ -0,0 +1,14 @@
+#include "losses.h"
+float cross_entropy_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols)
+{
+    float loss = 0.0f;
+    float temp = 0.0f;
+    for (int i = 0; i < Y_rows; i++) {
+        temp = 0.0f;
+        for (int j = 0; j < Y_cols; j++) {
+            temp += Y_true[i][j]*log(Y_pred[i][j]);
+        }
+        loss += temp / Y_rows;
+    }
+    return loss;
+}
---
+++ NeuralNetwork_with_CUDA/losses.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <math.h>
+
+float cross_entropy_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols);
