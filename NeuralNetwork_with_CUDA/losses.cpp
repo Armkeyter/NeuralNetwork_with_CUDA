a9ec8e365f3c24810a9ef91a94e0a790cc7bbf46
mhurte <37448105+mhurte@users.noreply.github.com>
2023-11-29 12:59:29 +0100

    losses

--- NeuralNetwork_with_CUDA/losses.cpp
+++ NeuralNetwork_with_CUDA/losses.cpp
@@ -12,3 +12,31 @@ float cross_entropy_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols)
     }
     return loss;
 }
+
+float mse_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols)
+{
+    float loss = 0.0f;
+    float temp = 0.0f;
+    for (int i = 0; i < Y_rows; i++) {
+        temp = 0.0f;
+        for (int j = 0; j < Y_cols; j++) {
+            temp += pow((Y_true[i][j]-(Y_pred[i][j])),2);
+        }
+        loss += temp / Y_rows;
+    }
+    return loss;
+}
+
+float mae_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols)
+{
+    float loss = 0.0f;
+    float temp = 0.0f;
+    for (int i = 0; i < Y_rows; i++) {
+        temp = 0.0f;
+        for (int j = 0; j < Y_cols; j++) {
+            temp += abs((Y_true[i][j]-(Y_pred[i][j])));
+        }
+        loss += temp / Y_rows;
+    }
+    return loss;
+}
--- NeuralNetwork_with_CUDA/losses.h
+++ NeuralNetwork_with_CUDA/losses.h
@@ -2,3 +2,5 @@
 #include <math.h>
 
 float cross_entropy_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols);
+float mse_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols);
+float mae_loss(int** Y_true, float** Y_pred, int Y_rows, int Y_cols);
